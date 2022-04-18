9779482d015a03483134149957f8a9dc7a39f5b3
torys <torysciacca@gmail.com>
2022-04-18 18:35:14 +1000

    Version 1.2

--- PaperScissorsRock.js
+++ PaperScissorsRock.js
@@ -1,6 +1,12 @@
-let playerScore = 0
-let computerScore = 0
+//A modified version of the Odin Project Rock,Paper,Scissors assignment by Tory Sciacca
+//This version seeks to make small improvements as well as a option to set the round number
 
+//Establishes global variables 
+let playerScore = 0;
+let computerScore = 0;
+let bestTo;
+
+//Function to generate the computers logic. At random choses between paper, scissors or rock
 function computerPlay(){
     
     let numGen = Math.floor((Math.random() * 3))
@@ -15,45 +21,56 @@ function computerPlay(){
 
 };
 
+//Round setup - Core game Loop
 function playRound(playerSelection, computerSelection){
     
-    if (playerSelection === "rock" && computerSelection === "paper") {
+    if ((playerSelection === "rock" && computerSelection === "paper") || 
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")) {
         computerScore ++
         return `You Lose! ${computerSelection} beats ${playerSelection}`
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
         playerScore ++
-        return `You Win! ${playerSelection} beats ${computerSelection}` 
-    } else if (playerSelection === "paper" && computerSelection === "scissors"){
-        computerScore ++
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
-        playerScore ++
-        return `You Win! ${playerSelection} beats ${computerSelection}`  
-    } else if (playerSelection === "scissors" && computerSelection === "rock"){
-        computerScore ++
-        return `You Lose! ${computerSelection} beats ${playerSelection}` 
-    } else if (playerSelection === "scissors" && computerSelection === "paper"){
-        playerScore ++
-        return `You Win! ${playerSelection} beats ${computerSelection}`     
+        return `You Win! ${playerSelection} beats ${computerSelection}`      
     } else if (playerSelection === computerSelection){
         return `Draw! You both picked ${playerSelection}`
+    } else {
+        return 'Try again, input not accepted.'
     }
 }
 
+//Consolidates functions into a game function
 function game(){
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Paper, Scissors or Rock?");
-        const computerSelection = computerPlay();
+    
+    //Asks user what score they want to play to
+    bestTo = prompt('Best to what?')
 
-        console.log(playRound(playerSelection.toLowerCase(),computerSelection))
+    //A while loop that checks that the input is a usable number
+    while(true){
+        if (isNaN(bestTo)){
+            console.log('Try agian, please pick a number.')
+            bestTo = prompt('What do you want to play to?')  
+        } else {break}
     }
-    if (playerScore === computerScore){ 
-        console.log("GAME OVER, DRAW")
-    } else if (playerScore > computerScore) {
-        console.log("GAME OVER, YOU WIN!")
-    } else if (playerScore < computerScore) {
-        console.log("GAME OVER, YOU LOSE!")
+
+    //Loops till someone wins
+    while (true){
+        if (playerScore >= bestTo){
+            console.log("GAME OVER, YOU WIN!")
+            console.log(`FINAL SCORE: You: ${playerScore}, Computer: ${computerScore}`)
+            break
+        } else if (computerScore >= bestTo) {
+            console.log("GAME OVER, YOU LOSE!")
+            console.log(`FINAL SCORE: You: ${playerScore}, Computer: ${computerScore}`)
+            break
+        } else {        
+            let playerSelection = prompt("Paper, Scissors or Rock?");
+            const computerSelection = computerPlay();
+
+            console.log(playRound(playerSelection.toLowerCase(),computerSelection))
+            console.log(`${playerScore}:${computerScore}`)
+        }
     }
 }
-
-game();
